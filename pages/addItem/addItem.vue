bc9f3c2a86c2458d8ce9f36344b8e3f6ec22b6c9
490144765 <164396438@qq.com>
2021-03-02 08:45:47 +0800

    first commit 3 pages without interface

---
+++ pages/addItem/addItem.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view>
+			<u-input v-model="name" :type="type" :border="border"  />
+		</view>
+		<view>
+			<u-input v-model="price" :type="type" :border="border"  />
+		</view>
+		<view>
+			<u-input v-model="amount" :type="type" :border="border"  />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '请输入入库商品信息',
+				name: '',
+				type: 'text',
+				border: true,
+				price: '',
+				amount: ''
+			}
+		},
+		methods: {
+			
+		}
+	};
+	console.log("hey");
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view>
+			<u-input v-model="value" :type="type" :border="border"  />
+		</view>
+		<view>
+			<u-input v-model="pValue" :type="ptype" :border="pborder" :password-icon="passwordIcon" />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '请登录',
+				value: '',
+				type: 'text',
+				border: true,
+				pValue: '',
+				ptype: 'password',
+				passwordIcon: true,
+				pborder: true,
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
